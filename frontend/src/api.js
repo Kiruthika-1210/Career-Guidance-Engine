69e5d54ccfeee287267b848e88add6c6c9fded84
Kiruthika-1210 <kiruthika.shalinimanivannan@gmail.com>
2026-01-12 19:24:48 +0530

    API url changed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,5 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-console.log("API BASE URL:", API_BASE_URL); 
-
 export async function sendChat(formData) {
   const res = await fetch(`${API_BASE_URL}/chat`, {
     method: "POST",
